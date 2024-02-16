c4a4be686c7e64bead91c82a25473d63eb2fdd31
Evgeny Zaryanov <eugenezaryanov@yandex.ru>
2024-02-16 18:01:43 +0300

    parallax-updated

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function parallax(event) {
   let mouseX = event.clientX;
+  let mouseY = event.clientY;
+
   if (event.type === 'touchmove') {
     mouseX = event.touches[0].clientX;
+    mouseY = event.touches[0].clientY;
   }
 
-  this.querySelectorAll('.layer').forEach(layer => {
-    let speed = layer.getAttribute('data-speed');
-    layer.style.transform = `translateX(${(mouseX * speed) / 1200}px)`;
+  this.querySelectorAll('.scene__layer').forEach(layer => {
+    let speedX = layer.getAttribute('data-speed-x'); // Скорость по X
+    let speedY = layer.getAttribute('data-speed-y'); // Скорость по Y
+    layer.style.transform = `translateX(${(mouseX * speedX) / 1200}px) translateY(${
+      (mouseY * speedY) / 1200
+    }px)`;
   });
 }
 document.addEventListener('mousemove', parallax);
